3fa25b3868a7347edccfd075f83bb5d85c59fd4d
Kyle-the-coder <kylethacoder@gmail.com>
2025-01-13 11:58:44 -0700

    updated

---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,8 @@
+import logo from "../../assets/THRIVELogo.png";
+export function Nav() {
+  return (
+    <div className="nav-mc">
+      <img className="logo-img" />
+    </div>
+  );
+}
--- src/layout/NavLayout.jsx
+++ src/layout/NavLayout.jsx
@@ -1,10 +1,12 @@
 import { Outlet, useLocation, useNavigation } from "react-router-dom";
+import { Nav } from "../components/Nav/Nav";
 
 export function NavLayout() {
   const { state } = useNavigation();
 
   return (
     <div className="main-container">
+      <Nav />
       {state === "loading" ? (
         "loading..."
       ) : (
