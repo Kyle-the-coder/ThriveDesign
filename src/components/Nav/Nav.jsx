4d7f490b8c3f91d6916bc6ef2c8207816709c209
Kyle-the-coder <kylethacoder@gmail.com>
2025-01-13 12:52:22 -0700

    updated

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,9 +1,24 @@
 import logo from "../../assets/THRIVELogo.png";
 import "./nav.css";
 export function Nav() {
+  const links = [
+    { linkName: "About Me", link: "/aboutme" },
+    { linkName: "Projects", link: "/projects" },
+    { linkName: "Services", link: "/services" },
+    { linkName: "Contact", link: "/contact" },
+  ];
   return (
     <div className="nav-mc">
       <img src={logo} className="logo-img" />
+      <div className="nav-links-container">
+        {links.map((link, index) => {
+          return (
+            <h3 key={link.linkName} className="font1 link">
+              {link.linkName}
+            </h3>
+          );
+        })}
+      </div>
     </div>
   );
 }
