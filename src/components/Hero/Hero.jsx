92c39f1dda18081dbb29cbd86baa62ab2b9e071a
Kyle-the-coder <kylethacoder@gmail.com>
2025-01-14 12:13:40 -0700

    updated

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,17 +1,34 @@
+import { useState } from "react";
 import heroBg from "../../assets/AbstractPlay.jpg";
 import "./hero.css";
 export function Hero() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
   return (
     <div className="hero-mc">
       <img src={heroBg} className="hero-bg" />
 
       <div className="hero-text">
-        <h1 className="blockTextThick font1Bold">
-          Websites <span className="orangeFont">built</span> to{" "}
-        </h1>
-        <h1 className="blockTextThick font1Bold">
-          grow with <span className="blueFont">you</span>
-        </h1>
+        {windowWidth <= 600 ? (
+          <>
+            {" "}
+            <h1 className="blockTextThick font1Bold">Websites</h1>
+            <h1 className="blockTextThick font1Bold orangeFont">built</h1>
+            <h1 className="blockTextThick font1Bold">to grow</h1>
+            <h1 className="blockTextThick font1Bold">
+              with <span className="blueFont">you</span>
+            </h1>
+          </>
+        ) : (
+          <>
+            {" "}
+            <h1 className="blockTextThick font1Bold">
+              Websites <span className="orangeFont">built</span> to{" "}
+            </h1>
+            <h1 className="blockTextThick font1Bold">
+              grow with <span className="blueFont">you</span>
+            </h1>
+          </>
+        )}
       </div>
 
       <div className="hero-blerb">
