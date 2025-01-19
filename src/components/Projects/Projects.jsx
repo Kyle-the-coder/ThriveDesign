eb00df1813cc5129ad9e153f478d824c28cf1813
Kyle-the-coder <kylethacoder@gmail.com>
2025-01-19 11:11:10 -0700

    updated

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -10,48 +10,72 @@ export function Projects() {
   return (
     <div className="projects-mc">
       <div className="projects-title">
-        <h1 className="blockTextThick font1Bold">Projects</h1>
+        <h1 className="whiteFont font1Bold">Projects</h1>
       </div>
 
       <div className="projects-display-container">
-        <div className="projects-container">
+        <div className="projects-container-1">
           <div className="dark-bg"></div>
           <img src={APC} className="projects-img" />
-          <h1 className="blockTextThick font1Bold">Restaurant</h1>
-          <p className="blockText font1">
-            Small Restaurant website created to showcase a custom review system
-          </p>
-          <ButtonBlue word="See Website" fontSize="clamp(1rem, 2.5vw, 3rem)" />
+          <div className="project-info">
+            <h1 className="whiteFont font1Bold">Restaurant</h1>
+            <p className="whiteFont font1Bold">
+              Small Restaurant website created to showcase a custom review
+              system
+            </p>
+            <ButtonBlue
+              word="See Website"
+              fontSize="clamp(1rem, 2.5vw, 2.5rem)"
+            />
+          </div>
         </div>
 
-        <div className="projects-container">
+        <div className="projects-container-2">
           <div className="dark-bg"></div>
           <img src={RJP} className="projects-img" />
-          <h1 className="blockTextThick font1Bold">Restaurant</h1>
-          <p className="blockText font1">
-            Small Restaurant website created to showcase a custom review system
-          </p>
-          <ButtonRed word="See Website" fontSize="clamp(1rem, 2.5vw, 3rem)" />
+          <div className="project-info">
+            <h1 className="whiteFont font1Bold">Retail</h1>
+            <p className="whiteFont font1Bold">
+              A Jewelry Store website made to showcase some advance features
+              such as stock search and attractive UI techniques
+            </p>
+            <ButtonRed
+              word="See Website"
+              fontSize="clamp(1rem, 2.5vw, 2.5rem)"
+            />
+          </div>
         </div>
 
-        <div className="projects-container">
+        <div className="projects-container-3">
           <div className="dark-bg"></div>
           <img src={Poke} className="projects-img" />
-          <h1 className="blockTextThick font1Bold">Restaurant</h1>
-          <p className="blockText font1">
-            Small Restaurant website created to showcase a custom review system
-          </p>
-          <ButtonBlue word="See Website" fontSize="clamp(1rem, 2.5vw, 3rem)" />
+          <div className="project-info">
+            <h1 className="whiteFont font1Bold">Game</h1>
+            <p className="whiteFont font1Bold">
+              A Pokemon Capture Card game created to show some complex systems
+              created in React{" "}
+            </p>
+            <ButtonBlue
+              word="See Website"
+              fontSize="clamp(1rem, 2.5vw, 2.5rem)"
+            />
+          </div>
         </div>
 
-        <div className="projects-container">
+        <div className="projects-container-4">
           <div className="dark-bg"></div>
           <img src={RMS} className="projects-img" />
-          <h1 className="blockTextThick font1Bold">Restaurant</h1>
-          <p className="blockText font1">
-            Small Restaurant website created to showcase a custom review system
-          </p>
-          <ButtonRed word="See Website" fontSize="clamp(1rem, 2.5vw, 3rem)" />
+          <div className="project-info">
+            <h1 className="whiteFont font1Bold">Service</h1>
+            <p className="whiteFont font1Bold">
+              A Personal Trainer website created with a Calendar to create a
+              schedule that clients can utilize to sign up for private sessions
+            </p>
+            <ButtonRed
+              word="See Website"
+              fontSize="clamp(1rem, 2.5vw, 2.5rem)"
+            />
+          </div>
         </div>
       </div>
     </div>
