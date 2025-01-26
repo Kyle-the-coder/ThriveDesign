5d3084c8913be12cf5f3077c1715bd97c1d622d6
Kyle-the-coder <kylethacoder@gmail.com>
2025-01-26 08:38:12 -0700

    updated

--- src/components/ProjectDisplay/ProjectDisplay.jsx
+++ src/components/ProjectDisplay/ProjectDisplay.jsx
@@ -1,4 +1,26 @@
+import appleVid from "../../assets/AppleVid.mp4";
+import { ButtonRed } from "../Button/ButtonRed";
 import "./projectdisplay.css";
-export function ProjectDisplay() {
-  return <div className="project-display-mc"></div>;
+export function ProjectDisplay({ title, desc, vid }) {
+  return (
+    <div className="project-display-mc">
+      <div className="display-container">
+        <div className="display-title">
+          <h1 className="blockText font1Bold">{title}</h1>
+        </div>
+        <div className="display-content">
+          <div className="display-vid-container">
+            <video controls src={vid} className="display-vid" />
+          </div>
+          <div className="display-info">
+            <p className="font1Bold">{desc}</p>
+            <ButtonRed
+              word="See Website"
+              fontSize="clamp(.8rem, 1vw, 1.8rem)"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/pages/ProjectPage/ProjectPage.jsx
+++ src/pages/ProjectPage/ProjectPage.jsx
@@ -1,13 +1,32 @@
 import { Title } from "../../components/Title/Title";
 import { Contact } from "../../components/Contact/Contact";
 import contactBg from "../../assets/AbstractFierceUP.jpg";
+import appleVid from "../../assets/AppleVid.mp4";
+import serviceVid from "../../assets/ServiceVid.mp4";
 import { ProjectDisplay } from "../../components/ProjectDisplay/ProjectDisplay";
 import "./projectpage.css";
 function ProjectPage() {
   return (
     <div className="project-page-mc">
       <Title title="Projects" />
-      <ProjectDisplay />
+
+      <ProjectDisplay
+        title="Restaurant"
+        desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.
+        Nisi ut aliquip ex ea commodo consequat."
+        vid={appleVid}
+      />
+      <ProjectDisplay
+        title="Service"
+        desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.
+        Nisi ut aliquip ex ea commodo consequat."
+        vid={serviceVid}
+      />
+
       <div className="project-contact-container">
         <img src={contactBg} className="project-contact-bg" />
         <Contact />
