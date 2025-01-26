6e9ca0f390061345a9a6e98dc843908f7f6954cc
Kyle-the-coder <kylethacoder@gmail.com>
2025-01-26 08:43:49 -0700

    updated

--- src/pages/ProjectPage/ProjectPage.jsx
+++ src/pages/ProjectPage/ProjectPage.jsx
@@ -3,6 +3,7 @@ import { Contact } from "../../components/Contact/Contact";
 import contactBg from "../../assets/AbstractFierceUP.jpg";
 import appleVid from "../../assets/AppleVid.mp4";
 import serviceVid from "../../assets/ServiceVid.mp4";
+import jewelVid from "../../assets/JewelVid.mp4";
 import { ProjectDisplay } from "../../components/ProjectDisplay/ProjectDisplay";
 import "./projectpage.css";
 function ProjectPage() {
@@ -25,6 +26,14 @@ function ProjectPage() {
         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.
         Nisi ut aliquip ex ea commodo consequat."
         vid={serviceVid}
+      />
+      <ProjectDisplay
+        title="Retail"
+        desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.
+        Nisi ut aliquip ex ea commodo consequat."
+        vid={jewelVid}
       />
 
       <div className="project-contact-container">
