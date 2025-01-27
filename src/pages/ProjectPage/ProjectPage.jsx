e0588d6c83ee74b2f82f3aae1dc70dac97abb1a9
Kyle-the-coder <kylethacoder@gmail.com>
2025-01-27 10:13:14 -0700

    updated

--- src/pages/ProjectPage/ProjectPage.jsx
+++ src/pages/ProjectPage/ProjectPage.jsx
@@ -1,10 +1,11 @@
 import { Title } from "../../components/Title/Title";
 import { Contact } from "../../components/Contact/Contact";
+import { ProjectDisplay } from "../../components/ProjectDisplay/ProjectDisplay";
 import contactBg from "../../assets/AbstractFierceUP.jpg";
 import appleVid from "../../assets/AppleVid.mp4";
 import serviceVid from "../../assets/ServiceVid.mp4";
 import jewelVid from "../../assets/JewelVid.mp4";
-import { ProjectDisplay } from "../../components/ProjectDisplay/ProjectDisplay";
+import pokeVid from "../../assets/pokeVid.mp4";
 import "./projectpage.css";
 function ProjectPage() {
   return (
@@ -28,6 +29,15 @@ function ProjectPage() {
         Nisi ut aliquip ex ea commodo consequat."
         vid={jewelVid}
         web="https://jewelrystore-c8dfc.web.app/"
+      />
+      <ProjectDisplay
+        title="Game"
+        desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.
+        Nisi ut aliquip ex ea commodo consequat."
+        vid={pokeVid}
+        web="https://pokemonstay-5cade.web.app/"
       />
       <ProjectDisplay
         title="Restaurant"
