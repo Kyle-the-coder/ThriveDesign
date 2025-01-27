0c72bfbe6ceb585ec59bf05d8fce0af0bc9b99d5
Kyle-the-coder <kylethacoder@gmail.com>
2025-01-27 10:57:56 -0700

    updated

---
+++ src/components/Price/Price.jsx
@@ -0,0 +1,11 @@
+export function Price() {
+  return (
+    <div className="price-mc">
+      <h1 className="font1Bold">Pricing</h1>
+      <div className="price-card-container">
+        <div className="price-card"></div>
+        <div className="price-card"></div>
+      </div>
+    </div>
+  );
+}
